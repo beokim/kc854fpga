76b04e6aaafa9b05db17de44916a758ddbaaa70f
MiK <MiK@example.com>
2015-12-09 13:12:03 +0100

    besseren Interruptcontroller verwenden

--- tape/transfer.sh
+++ tape/transfer.sh
@@ -8,8 +8,8 @@ stty -F /dev/ttyS0 2400
 # printf "\x1b\x54\x00\x03\x00\x0c" >/dev/ttyS0
 # dd if=ALLIENS.KCC bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
 
-# printf "\x1b\x54\x00\x02\x80\x3f" >/dev/ttyS0
-# dd if=DIGGER.KCC bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
+printf "\x1b\x54\x00\x02\x80\x3f" >/dev/ttyS0
+dd if=DIGGER.KCC bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
 
 # printf "\x1b\x54\x00\x02\x80\x50" >/dev/ttyS0
 # dd if=DIGGER-4.KCC bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
@@ -17,5 +17,5 @@ stty -F /dev/ttyS0 2400
 # printf "\x1b\x54\x00\x02\x00\x05" >/dev/ttyS0
 # dd if=LIFE.KCC bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
 
-printf "\x1b\x54\x00\x30\x00\x41" >/dev/ttyS0
-dd if=sokoban4.kcc bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
+# printf "\x1b\x54\x00\x30\x00\x41" >/dev/ttyS0
+# dd if=sokoban4.kcc bs=128 skip=1 status=noxfer  2> /dev/null  >/dev/ttyS0
